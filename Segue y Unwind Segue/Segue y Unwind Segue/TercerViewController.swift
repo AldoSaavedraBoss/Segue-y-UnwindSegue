94f69d055f9f3d47a2374d2ce80af5f73e8ee8c3
Aldo Saavedra Cervantes <aldo_saavedra_cervantes@outlook.com>
2021-09-18 22:16:17 -0500

    primer commit

---
+++ Segue y Unwind Segue/Segue y Unwind Segue/TercerViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TercerViewController.swift
+//  Segue y Unwind Segue
+//
+//  Created by Mac20 on 18/09/21.
+//
+
+import UIKit
+
+class TercerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func RegresarButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Segue y Unwind Segue/Segue y Unwind Segue/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Segue y Unwind Segue
+//
+//  Created by Mac20 on 18/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToOne(_ sender: UIStoryboardSegue){
+        
+    }
+
+}
+
